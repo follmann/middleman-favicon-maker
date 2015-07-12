63a1fe6d2f3f621b8156b355fe432e3198071a5d
Andreas Follmann <andreas@toyrocketscience.com>
2015-07-12 17:05:56 +0200

    beta release v4.0.0.beta.1 along middleman v4 betas

--- lib/middleman-favicon-maker/version.rb
+++ lib/middleman-favicon-maker/version.rb
@@ -1,9 +1,9 @@
 module Middleman
   module FaviconMaker
-    MAJOR = 3
-    MINOR = 7
-    PATCH = nil
-    BUILD = nil
+    MAJOR = 4
+    MINOR = 0
+    PATCH = 0
+    BUILD = "beta.1"
 
     VERSION = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
