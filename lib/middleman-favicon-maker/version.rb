cb904364aa0daf1e716038fe9ab726791e8f3348
Andreas Follmann <andreas@toyrocketscience.com>
2015-11-23 11:14:33 +0100

    bump version accordingly (still relies on beta release of MM4)

--- lib/middleman-favicon-maker/version.rb
+++ lib/middleman-favicon-maker/version.rb
@@ -2,7 +2,7 @@ module Middleman
   module FaviconMaker
     MAJOR = 4
     MINOR = 0
-    PATCH = 0
+    PATCH = 1
     BUILD = "beta.1"
 
     VERSION = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
